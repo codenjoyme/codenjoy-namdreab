2b10b21791c92198d57f2dddca6532c5e84629ff
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-12 13:36:05 +0200

    [engine][namdreab][clifford] Refactoring. Extracted performance test logic from Clifford to Engine. Use it in namdreab game.

---
+++ src/test/java/com/codenjoy/dojo/namdreab/PerformanceTest.java
@@ -0,0 +1,61 @@
+package com.codenjoy.dojo.namdreab;
+
+/*-
+ * #%L
+ * Codenjoy - it's a dojo-like platform from developers to developers.
+ * %%
+ * Copyright (C) 2012 - 2022 Codenjoy
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.codenjoy.dojo.namdreab.services.GameRunner;
+import com.codenjoy.dojo.namdreab.services.GameSettings;
+import org.junit.Test;
+
+import static com.codenjoy.dojo.services.round.RoundSettings.Keys.ROUNDS_ENABLED;
+import static com.codenjoy.dojo.utils.TestUtils.assertPerformance;
+
+public class PerformanceTest {
+
+    @Test
+    public void test() {
+
+        // about 8 sec
+        int expectedPrint = 4500;
+        int expectedTick = 2000;
+        int expectedCreation = 1000;
+
+        int players = 5;
+        int ticks = 10000;
+
+        GameRunner runner = new GameRunner(){
+            @Override
+            public GameSettings getSettings() {
+                return new GameSettings()
+                        .bool(ROUNDS_ENABLED, false);
+            }
+        };
+
+        boolean printBoard = false;
+        assertPerformance(runner,
+                players, ticks,
+                expectedPrint, expectedTick, expectedCreation,
+                printBoard);
+    }
+
+
+}
